0f9e02eda9278fb95364ff636a9fbe2ff6a03bdf
johnnymayer <johnnymayer@gmail.com>
2018-01-10 14:40:07 -0800

    Add form fields for sender, sender city

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userNameInput = $("input#userName").val();
+    var senderNameInput = $("input#senderName").val();
+    var senderCityInput = $("input#senderCity").val();
 
     $(".userName").text(userNameInput);
+    $(".senderName").text(senderNameInput);
+    $(".senderCity").text(senderCityInput);
 
     $("#postcard").show();
 
